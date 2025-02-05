de135d57277c4dc649d4d255f40e294a50d67b30
AyoubMDL <ayoubbih119@gmail.com>
2025-02-05 23:25:26 +0100

    feat: common utils

--- day001/vector_addition.cu
+++ day001/vector_addition.cu
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <cstdlib>
 #include <ctime>
+#include "common.h"
 
 
 // CUDA kernel for vector addition
@@ -20,35 +21,6 @@ void vectorAddCPU(const float *A, const float *B, float *C, int N) {
     for (int i = 0; i < N; i++) {
         C[i] = A[i] + B[i];
     }
-}
-
-
-bool compareResults(const float *A, const float *B, int N) {
-    for (int i = 0; i < N; i++) {
-        if (fabs(A[i] - B[i]) > 1e-5) {
-            std::cout << "Mismatch at index " << i << ": CPU=" << A[i] << " GPU=" << B[i] << std::endl;
-            return false;
-        }
-    }
-    return true;
-}
-
-
-void initializeVectors(float *A, float *B, int N) {
-    srand(static_cast<unsigned int>(time(0)));
-    for (int i = 0; i < N; i++) {
-        A[i] = static_cast<float>(rand()) / RAND_MAX;
-        B[i] = static_cast<float>(rand()) / RAND_MAX;
-}
-
-
-template <typename Func>
-double measureExecutionTime(Func func) {
-    auto start = std::chrono::high_resolution_clock::now();
-    func();
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> duration = end - start;
-    return duration.count();
 }
 
 
